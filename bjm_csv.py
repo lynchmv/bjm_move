be943e9f4ec50beaf161b0b6762a19a15d69fb75
Michael Lynch <lynchmv@gmail.com>
2021-02-16 20:26:03 -0500

    Add script to create csv

---
+++ bjm_csv.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+""" Script to build and populate Ben Jones database """
+# pylint: disable=line-too-long
+
+import csv
+import datetime
+import logging
+import requests
+import urllib3
+import yaml
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+CONFIG_FILE = open('db_config.yml', 'r')
+CONFIG_DATA = yaml.load(CONFIG_FILE, Loader=yaml.FullLoader)
+
+LOGLEVEL = CONFIG_DATA['log_lvl'].upper()
+logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=LOGLEVEL)
+
+CATEGORY_URL = 'https://www.benjones.com/api/categories.php'
+MACHINE_URL = 'https://www.benjones.com/api/machines.php'
+PICTURE_URL = 'https://www.benjones.com/api/pictures.php'
+VIDEO_URL = 'https://www.benjones.com/api/videos.php'
+PARAMS_DICT = {'key': CONFIG_DATA['api_key']}
+
+CURRENT_TIME = datetime.datetime.now()
+
+VERIFY_SSL = True
+
+def write_header(line):
+    with open('products_import.csv', 'w') as file:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerow(line)
+    return
+
+def write_line(line):
+    with open('products_import.csv', 'a+') as file:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerow(line)
+    return
+
+def get_categories():
+    """ Get categories """
+    response = requests.get(CATEGORY_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    if response.status_code != 200:
+        raise RuntimeError('GET /categories/ {}'.format(response.status_code))
+    for category in response.json():
+        get_category(category['cid'], category['name'])
+
+def get_category(category_id, category_name):
+    """ Get machines from a category """
+    PARAMS_DICT.update({'cid': category_id})
+    response = requests.get(CATEGORY_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    for machine in response.json():
+        get_machine(machine['mid'], category_id, category_name)
+
+def inventory_status(sold):
+    logging.debug(f"Machine sold status: {sold}")
+    result = "unknown"
+    if sold == '0':
+        result = "instock"
+    if sold == '1':
+        result = "onbackorder"
+    if sold == '2':
+        result = "outofstock"
+    logging.debug(f"inventory_status result: {result}")
+    return result
+
+def get_machine(machine_id, category_id, category_name):
+    """ Get machine details """
+    PARAMS_DICT.update({'mid': machine_id})
+    machine_info = requests.get(MACHINE_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    machine_csv = []
+    machine_csv.append("simple") # Type
+    for machine in machine_info.json():
+        machine_csv.append(machine['mid']) # SKU
+        logging.debug(f"Machine ID: {machine['mid']}")
+        if machine['name'] is None: # Name
+            machine_csv.append('Unknown')
+        else:
+            machine_csv.append(machine['name'].title())
+        logging.debug(f"Machine name: {machine['name']}")
+        machine_csv.append(1) # Published
+        machine_csv.append(0) # Is Featured
+        machine_csv.append("visible") # Visibility in catalog
+        machine_csv.append(machine['descr']) # Description
+        logging.debug(f"Machine description: {machine['descr']}")
+        machine_csv.append("taxable") # Tax status
+        machine_csv.append(1) # In stock?
+        machine_csv.append(0) # Backorders allowed?
+        machine_csv.append(0) # Sold individually
+        machine_csv.append(0) # Allow customer reviews
+        machine_csv.append(machine['price']) # Regular price
+        logging.debug(f"Machine price: {machine['price']}")
+        machine_csv.append(category_name.title()) # Categories
+        logging.debug(f"Machine category: {category_name.title()}")
+    machine_pics = requests.get(PICTURE_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    picture_list = []
+    try:
+        for picture in machine_pics.json():
+            picture_list.append("https://benjones.com/machines/" + picture['name'])
+    except StopIteration:
+        pass
+    machine_vids = requests.get(VIDEO_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    try:
+        for video in machine_vids.json():
+            picture_list.append(video['link'])
+    except StopIteration:
+        pass
+    if picture_list:
+        import_pics = ', '.join(picture_list)
+        machine_csv.append(import_pics) # Images
+    else:
+        machine_csv.append('')
+    logging.debug(f"Machine pictures: {picture_list}")
+    machine_csv.append(0) # Position
+    if machine['mfg'] is None: # Brand
+        machine_csv.append('Unknown')
+    else:
+        machine_csv.append(machine['mfg'].title())
+    logging.debug(f"Machine manufacturer: {machine['mfg']}")
+    machine_csv.append(machine['notes'] or ' ') # _pans_ta
+    logging.debug(f"Machine notes: {machine['notes']}")
+    machine_csv.append(machine['cost'] or '0') # _wc_cog_cost
+    machine_csv.append(machine['owned'] or '0') # _bjm_owned
+    machine_csv.append(machine['rigging'] or '0') # _wc_cor_cost
+    machine_csv.append(machine['sold']) # _bjm_sold
+    machine_csv.append(inventory_status(machine['sold'])) # In stock?
+    write_line(machine_csv)
+
+header_line = ["Type","SKU","Name","Published","Is featured?","Visibility in catalog","Description","Tax status","In stock?","Backorders allowed?","Sold individually?","Allow customer reviews?","Regular price","Categories","Images","Position","Brand","Meta: _wp_page_template","Meta: _pans_ta","Meta: _wc_cog_cost","Meta: _bjm_owned","Meta: _wc_cor_cost","Meta: _bjm_sold","In Stock?"]
+write_header(header_line)
+get_categories()
--- bjm_db.py
+++ bjm_db.py
@@ -1,43 +1,51 @@
 #!/usr/bin/env python
-""" Script to get BenJones database """
+""" Script to build and populate Ben Jones database """
 # pylint: disable=line-too-long
 
 import datetime
 import requests
 import mysql.connector as mysql
+import urllib3
+import yaml
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-DB = mysql.connect(
-        host='localhost',
-        user='bjm_admin',
-        passwd='',
-        database='ben_jones'
-)
-CURSOR = DB.cursor()
+CONFIG_FILE = open('db_config.yml', 'r')
+CONFIG_DATA = yaml.load(CONFIG_FILE, Loader=yaml.FullLoader)
 
 CATEGORY_URL = 'https://www.benjones.com/api/categories.php'
 MACHINE_URL = 'https://www.benjones.com/api/machines.php'
 PICTURE_URL = 'https://www.benjones.com/api/pictures.php'
 VIDEO_URL = 'https://www.benjones.com/api/videos.php'
-PARAMS_DICT = {'key': ''}
+PARAMS_DICT = {'key': CONFIG_DATA['api_key']}
 
 CURRENT_TIME = datetime.datetime.now()
+
+VERIFY_SSL = True
+
+DB = mysql.connect(
+        host=CONFIG_DATA['db_host'],
+        user=CONFIG_DATA['db_user'],
+        passwd=CONFIG_DATA['db_pass'],
+        database=CONFIG_DATA['db_name']
+)
+CURSOR = DB.cursor()
 
 def create_tables(drop_first=0):
     """ Create database tables """
     if drop_first != 0:
-        drop_statement = "DROP TABLE vids"
+        drop_statement = "DROP TABLE IF EXISTS vids"
         CURSOR.execute(drop_statement)
-        drop_statement = "DROP TABLE pics"
+        drop_statement = "DROP TABLE IF EXISTS pics"
         CURSOR.execute(drop_statement)
-        drop_statement = "DROP TABLE machines"
+        drop_statement = "DROP TABLE IF EXISTS machines"
         CURSOR.execute(drop_statement)
-        drop_statement = "DROP TABLE categories"
+        drop_statement = "DROP TABLE IF EXISTS categories"
         CURSOR.execute(drop_statement)
 
     sql_statement = ("CREATE TABLE IF NOT EXISTS categories ( "
                      "cid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, "
                      "name varchar(255), "
-                     "created_at TIMESTAMP NOT NULL DEFAULT now(), "
+                     "created_at TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00', "
                      "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP "
                      ")")
     CURSOR.execute(sql_statement)
@@ -45,6 +53,7 @@ def create_tables(drop_first=0):
     sql_statement = ("CREATE TABLE IF NOT EXISTS machines ( "
                      "mid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, "
                      "cid INTEGER NOT NULL, "
+                     "scid INTEGER DEFAULT 0, "
                      "pid INTEGER NOT NULL, "
                      "name VARCHAR(255), "
                      "manufacturer VARCHAR(255), "
@@ -60,8 +69,9 @@ def create_tables(drop_first=0):
                      "pending TINYINT NOT NULL DEFAULT 0, "
                      "sold TINYINT NOT NULL DEFAULT 0, "
                      "listed VARCHAR(255), "
-                     "created_at TIMESTAMP NOT NULL DEFAULT now(), "
+                     "created_at TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00', "
                      "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+                     "INDEX (cid), "
                      "FOREIGN KEY (cid) REFERENCES categories (cid) "
                      "ON UPDATE CASCADE "
                      ")")
@@ -74,8 +84,9 @@ def create_tables(drop_first=0):
                      "title VARCHAR(255), "
                      "active TINYINT NOT NULL DEFAULT 0, "
                      "picorder INTEGER, "
-                     "created_at TIMESTAMP NOT NULL DEFAULT now(), "
+                     "created_at TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00', "
                      "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+                     "INDEX (mid), "
                      "FOREIGN KEY (mid) REFERENCES machines (mid) "
                      "ON UPDATE CASCADE "
                      ")")
@@ -85,8 +96,9 @@ def create_tables(drop_first=0):
                      "vid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, "
                      "mid INTEGER, "
                      "link VARCHAR(255), "
-                     "created_at TIMESTAMP NOT NULL DEFAULT now(), "
+                     "created_at TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00', "
                      "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+                     "INDEX (mid), "
                      "FOREIGN KEY (mid) REFERENCES machines (mid) "
                      "ON UPDATE CASCADE "
                      ")")
@@ -95,10 +107,11 @@ def create_tables(drop_first=0):
 
 def get_categories():
     """ Get categories """
-    response = requests.get(CATEGORY_URL, params=PARAMS_DICT)
+    response = requests.get(CATEGORY_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
     if response.status_code != 200:
         raise RuntimeError('GET /categories/ {}'.format(response.status_code))
-    sql_statement = "INSERT INTO categories VALUES (%s, %s, now(), now())"
+    sql_statement = ("INSERT INTO categories VALUES (%s, %s, now(), now()) "
+                     "ON DUPLICATE KEY UPDATE updated_at = now()")
     for category in response.json():
         values = (int(category['cid']), category['name'])
         CURSOR.execute(sql_statement, values)
@@ -108,31 +121,32 @@ def get_categories():
 def get_category(category_id):
     """ Get machines from a category """
     PARAMS_DICT.update({'cid': category_id})
-    response = requests.get(CATEGORY_URL, params=PARAMS_DICT)
+    response = requests.get(CATEGORY_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
     for machine in response.json():
         get_machine(machine['mid'], category_id)
 
 def get_machine(machine_id, category_id):
     """ Get machine details """
     PARAMS_DICT.update({'mid': machine_id})
-    machine_info = requests.get(MACHINE_URL, params=PARAMS_DICT)
-    sql_statement = ("REPLACE INTO machines VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, "
-                     "%s, %s, %s, %s, %s, %s, %s, %s, %s, now())")
+    machine_info = requests.get(MACHINE_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
+    sql_statement = ("INSERT INTO machines VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, "
+                     "%s, %s, %s, %s, %s, %s, %s, %s, %s, now()) ON DUPLICATE KEY "
+                     "UPDATE scid = %s, updated_at = now()")
     for machine in machine_info.json():
-        machine['pid'] = (0, machine['pid'])[type(machine['pid']) == 'str']
-        values = (int(machine['mid']), int(category_id), int(machine['pid']), machine['name'], machine['mfg'],
+        machine['pid'] = (0, machine['pid'])[isinstance(machine['pid'], str)]
+        values = (int(machine['mid']), int(category_id), int(category_id), int(machine['pid']), machine['name'], machine['mfg'],
                   machine['descr'], machine['notes'], machine['location'], machine['cost'], machine['rigging'],
                   machine['price'], machine['owned'], machine['active'], machine['feat'], machine['pending'],
-                  machine['sold'], machine['listed'], machine['added'])
+                  machine['sold'], machine['listed'], machine['added'], int(category_id))
         CURSOR.execute(sql_statement, values)
         DB.commit()
-    machine_pics = requests.get(PICTURE_URL, params=PARAMS_DICT)
+    machine_pics = requests.get(PICTURE_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
     try:
         for picture in machine_pics.json():
             sql_statement = f"INSERT INTO pics ( {picture['pid']}, {picture['mid']}, '{picture['name']}', '{picture['title']}', {picture['active']}, {picture['picorder']}, now(), now());\n"
     except StopIteration:
         pass
-    machine_vids = requests.get(VIDEO_URL, params=PARAMS_DICT)
+    machine_vids = requests.get(VIDEO_URL, params=PARAMS_DICT, verify=VERIFY_SSL)
     try:
         for video in machine_vids.json():
             sql_statement = f"INSERT INTO vids ({video['vid']}, {video['mid']}, '{video['link']}', now(), now());\n"
